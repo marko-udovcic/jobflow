4d0aee6daaf8749fe2716f008f3c70074a5971e7
marko udovcic <markoudovcic53@gmail.com>
2025-02-28 17:13:39 +0100

    feat: add backend

---
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/JobflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobflowApiApplication.class, args);
+	}
+
+}
---
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/controllers/TestController.java
@@ -0,0 +1,22 @@
+package com.example.jobflow_api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Map<String, String>> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from backend!");
+        return ResponseEntity.ok(response);  // Vraća JSON
+    }
+
+}
