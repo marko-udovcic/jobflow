75041c5c740ed2dd37a09db30027272b2152fda4
marko udovcic <markoudovcic53@gmail.com>
2025-03-01 17:25:02 +0100

    add and update frontend configuration files

--- frontend/jobflow-ui/vite.config.js
+++ frontend/jobflow-ui/vite.config.js
@@ -5,6 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // host: "0.0.0.0",
+    host: true,
     port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
   },
 });
