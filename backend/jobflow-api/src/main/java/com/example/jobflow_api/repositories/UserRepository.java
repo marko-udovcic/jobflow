1b846599c6f040f5fb34e07b359bb87cf809ae3e
marko udovcic <markoudovcic53@gmail.com>
2025-03-07 18:47:56 +0100

    refactor: change file name and update pom

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/UserRepository.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.jobflow_api.repositories;
 
 import com.example.jobflow_api.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AppUserRepository extends JpaRepository<AppUser, String> {
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, String> {
 }
