cb6ba97afbf890eb485b15d2dd5ad68227ce20dc
marko udovcic <markoudovcic53@gmail.com>
2025-03-08 11:23:45 +0100

    refactor: change configuration files

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/JobflowApiApplication.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/JobflowApiApplication.java
@@ -3,11 +3,14 @@ package com.example.jobflow_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.jobflow_api")
 public class JobflowApiApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobflowApiApplication.class, args);
+		System.out.println("Hello from development branch");
 	}
 
 }
