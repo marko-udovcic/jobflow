0e6664e585c00e72171984144b596c5ca4d13ac0
marko udovcic <markoudovcic53@gmail.com>
2025-03-13 10:26:22 +0100

    feat: add updateUser method(update company name and about company)

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/service/UserService.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.jobflow_api.service;
 
+import com.example.jobflow_api.dtos.UpdateCompanyRequest;
 import com.example.jobflow_api.dtos.UserDTO;
 import com.example.jobflow_api.models.AppUser;
 import com.example.jobflow_api.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,6 @@ public interface UserService {
 
 
     UserDTO getUserById(String id);
+
+    AppUser updateUser(UpdateCompanyRequest updateCompanyRequest, HttpServletRequest request);
 }
