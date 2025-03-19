8ee65a9e973592562b29d88d7a51a43a205db7d2
marko udovcic <markoudovcic53@gmail.com>
2025-03-19 23:57:18 +0100

    add entity graph

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/JobApplicationRepository.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/JobApplicationRepository.java
@@ -3,8 +3,15 @@ package com.example.jobflow_api.repositories;
 import com.example.jobflow_api.models.JobApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication,String> {
     Page<JobApplication> findByJobPostingId(String jobId, Pageable pageable);
+
+
+    @EntityGraph(value = "JobApplication.full",type = EntityGraph.EntityGraphType.LOAD)
+    List<JobApplication> findByWorkerId(String workerId);
 }
