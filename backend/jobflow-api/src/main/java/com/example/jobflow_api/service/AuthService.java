89bcef2e7ebfb1a6f7de6ebe70297f26a003a0fc
marko udovcic <markoudovcic53@gmail.com>
2025-03-22 17:32:42 +0100

    update authenticateUser method and add logoutUser method in AuthService interface

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/service/AuthService.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/service/AuthService.java
@@ -4,6 +4,7 @@ import com.example.jobflow_api.security.request.LoginRequest;
 import com.example.jobflow_api.security.request.SignupRequest;
 import com.example.jobflow_api.security.response.MessageResponse;
 import com.example.jobflow_api.security.services.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
@@ -16,7 +17,9 @@ public interface AuthService {
 
     Map<String, Object> validateSignupRequest(SignupRequest signupRequest, BindingResult bindingResult);
 
-    ResponseEntity<?> authenticateUser(LoginRequest loginRequest);
+    ResponseEntity<?> authenticateUser(LoginRequest loginRequest, HttpServletResponse response);
 
    UserDetailsImpl getCurrentUserDetails();
+
+    ResponseEntity<?> logoutUser(HttpServletResponse response);
 }
