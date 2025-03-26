6c02147c4def87248f005afae5b5462c03016f20
marko udovcic <markoudovcic53@gmail.com>
2025-03-26 10:43:28 +0100

    refactor: update Button ui style

--- frontend/jobflow-ui/src/components/ui/Button.jsx
+++ frontend/jobflow-ui/src/components/ui/Button.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 const variants = {
   primary: `bg-[#0e0e0e] text-white 
-    border-[#0e0e0e] border-1 hover:bg-transparent hover:text-black
+    border-[#0e0e0e] border-1 
     hover:border-[#0e0e0e] hover:border-1
-    rounded-[30px] p-2 self-center focus:outline-none mt-8 transition-all 
+    rounded-[30px] self-center focus:outline-none transition-all px-4 py-2 hover:bg-gray-800
     duration-200 ease-in-out`,
 
   secondary: `text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200 active:bg-gray-300 transition 
@@ -12,11 +12,12 @@ const variants = {
   danger: `text-red-500 p-2 mt-4 rounded lg:ml-4 transition duration-500 ease-in-out lg:hover:bg-red-700
             lg:hover:text-white`,
 };
+
 export default function Button({ children, onClick, className, disabled, variant, type }) {
   return (
     <button
       onClick={onClick}
-      className={`${className} ${variants[variant]}`}
+      className={`${variants[variant]} ${className} `}
       disabled={disabled}
       type={type}
     >
