feb5e4798c1ce750437fac6830140caf0e79f168
marko udovcic <markoudovcic53@gmail.com>
2025-03-27 12:21:23 +0100

    refactor: add variable inputVariant

--- frontend/jobflow-ui/src/components/ui/InputLabelField.jsx
+++ frontend/jobflow-ui/src/components/ui/InputLabelField.jsx
@@ -1,16 +1,17 @@
 import Input from "./Input";
 import ErrorSpan from "./ErrorSpan";
 import PropTypes from "prop-types";
-function InputLabelField({ name, labelName, type, formik, showError }) {
+function InputLabelField({ name, labelName, type, formik, showError, inputVariant = "primary" }) {
   return (
     <div className="mb-4">
       <label htmlFor={name} className="ml-2 block mb-1">
         <ErrorSpan name={name} labelName={labelName} showError={showError} />
       </label>
+
       <Input
         type={type}
         name={name}
-        variant="primary"
+        variant={inputVariant}
         className="w-full"
         value={formik.values[name]}
         onChange={formik.handleChange}
@@ -27,5 +28,6 @@ InputLabelField.propTypes = {
   type: PropTypes.string,
   formik: PropTypes.object.isRequired,
   showError: PropTypes.func.isRequired,
+  inputVariant: PropTypes.string,
 };
 export default InputLabelField;
