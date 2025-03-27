1ace5a71ec1e839f76f3b9551b0c5a7ecc9958d4
marko udovcic <markoudovcic53@gmail.com>
2025-03-27 12:18:31 +0100

    refactor: update front DockerFile and UpdateCompanyRequest dto

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/dtos/UpdateCompanyRequest.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/dtos/UpdateCompanyRequest.java
@@ -1,10 +1,12 @@
 package com.example.jobflow_api.dtos;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class UpdateCompanyRequest {
     private String companyName;
     private String aboutCompany;
