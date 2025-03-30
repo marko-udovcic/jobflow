d5c1ec5ebb18b211b869b7c7d3f10bbbd10f1c6b
marko udovcic <markoudovcic53@gmail.com>
2025-03-30 17:44:49 +0200

    refactor: add Navbar, Outletm useLocation for show Navbar in child components and useCurrentUser

--- frontend/jobflow-ui/src/App.jsx
+++ frontend/jobflow-ui/src/App.jsx
@@ -1,14 +1,16 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import moment from "moment";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useCurrentUser } from "./features/auth/hooks/useCurrentUser";
 function App() {
-  const [message, setMessage] = useState("hello");
+  const { isLoading } = useCurrentUser();
+  const location = useLocation();
+  const hideNavbarPaths = ["/worker/update-cv"];
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="app-container">
-      <Navbar />
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
       <Outlet />
     </div>
   );
