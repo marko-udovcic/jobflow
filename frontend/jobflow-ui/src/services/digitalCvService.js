66609dc36d215f6d2c6550c56edc027874ddebc0
marko udovcic <markoudovcic53@gmail.com>
2025-03-31 14:38:46 +0200

    feat: add categoryx and digitalCv service

---
+++ frontend/jobflow-ui/src/services/digitalCvService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export async function addDigitalCv(data) {
+  const response = await axiosInstance.post(`/digital-cv`, data);
+  return response.data;
+}
