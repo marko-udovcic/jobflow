3b7e9ecefe5b9f00660a044920bc96e3efba516c
marko udovcic <markoudovcic53@gmail.com>
2025-04-01 22:00:16 +0200

    refactor: add attributes jobPostingId and companyName to dto and JobApplicationServiceImpl

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/dtos/JobApplicationForWorkerDTO.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/dtos/JobApplicationForWorkerDTO.java
@@ -15,5 +15,7 @@ public class JobApplicationForWorkerDTO {
     private String applicationStatus;
     private String applicationDate;
     private List<JobMessageResponse> messages;
+    private String jobPostingId;
+    private String companyName;
 
 }
