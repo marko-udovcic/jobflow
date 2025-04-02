26f114db539a942bef386a9e9081261ee7624100
marko udovcic <markoudovcic53@gmail.com>
2025-04-02 20:35:21 +0200

    feat: add apiAuth services

---
+++ frontend/jobflow-ui/src/features/auth/services/apiAuth.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_AUTH_API_URL;
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export async function signUpUser(user) {
+  try {
+    const response = await axios.post(`${API_URL}/public/register`, user, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Registration failed:", error.response?.data || error.message);
+    throw error.response?.data || new Error("Failed to register");
+  }
+}
+
+export async function loginUser(credentials) {
+  try {
+    const response = await axiosInstance.post(`/public/login`, credentials);
+    return response.data;
+  } catch (error) {
+    console.error("Login failed:", error.response?.data || error.message);
+    throw error.response?.data || new Error("Failed to login");
+  }
+}
+
+export async function logoutUser() {
+  const response = await axiosInstance.post(`/logout`);
+  return response.data;
+}
+
+export async function getCurrentUser() {
+  const response = await axiosInstance.get("/current-user");
+  return response.data;
+}
