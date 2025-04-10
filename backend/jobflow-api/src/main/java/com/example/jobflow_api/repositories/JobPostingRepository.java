b1738f529f35a06f6f87112da42435c0ead0d2ec
marko udovcic <markoudovcic53@gmail.com>
2025-04-10 12:35:53 +0200

    refactor(repos): add @Repository annotations for all repo files

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/JobPostingRepository.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/JobPostingRepository.java
@@ -2,9 +2,11 @@ package com.example.jobflow_api.repositories;
 
 import com.example.jobflow_api.models.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface JobPostingRepository extends JpaRepository<JobPosting, String> {
     List<JobPosting> findByEmployerId(String employerId);
 }
