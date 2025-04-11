c09a6143304b641f85d61a1cca3ed48fe0da01b8
marko udovcic <markoudovcic53@gmail.com>
2025-04-11 20:14:28 +0200

    feat(controllers): add get mapping(search, suggest-locations...)

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/controllers/JobPostingController.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/controllers/JobPostingController.java
@@ -2,13 +2,18 @@ package com.example.jobflow_api.controllers;
 
 
 import com.example.jobflow_api.dtos.CreateJobPostingRequest;
+import com.example.jobflow_api.dtos.JobListingResponse;
 import com.example.jobflow_api.dtos.JobPostingDTO;
+import com.example.jobflow_api.dtos.PaginationDTO;
 import com.example.jobflow_api.models.JobPosting;
+import com.example.jobflow_api.models.indexes.JobPostingDocument;
 import com.example.jobflow_api.service.JobPostingService;
+import com.example.jobflow_api.service.JobSearchService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,13 +21,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@RequiredArgsConstructor
+import java.util.Set;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/job-postings")
 @SecurityRequirement(name = "bearerAuth")
 public class JobPostingController {
 
     private final JobPostingService jobPostingService;
+    private final JobSearchService jobSearchService;
 
     @PostMapping
     public ResponseEntity<JobPosting> createJobPost(@Valid @RequestBody CreateJobPostingRequest createJobPostingRequest,
@@ -53,4 +61,26 @@ public class JobPostingController {
     public ResponseEntity<?> deleteJobPosting(@PathVariable String id) {
         return jobPostingService.deleteJobPosting(id);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<PaginationDTO<JobListingResponse>> search(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String location,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        PaginationDTO<JobListingResponse> result = jobSearchService.searchJobPostings(title, location, page, size);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/suggest-locations")
+    public Set<String> suggestLocations(@RequestParam String prefix){
+        return jobSearchService.suggestLocations(prefix);
+    }
+
+    @GetMapping("/suggest-job-titles")
+    public Set<String> suggestJobTitles(@RequestParam String prefix){
+        return jobSearchService.suggestJobTitles(prefix);
+    }
+
 }
