2bb18611483f37f53e5abed70ff45be9c2d890fb
marko udovcic <markoudovcic53@gmail.com>
2025-04-15 21:57:00 +0200

    refactor: update Applications page(add WorkerApplications component and some ui)

--- frontend/jobflow-ui/src/pages/Applications.jsx
+++ frontend/jobflow-ui/src/pages/Applications.jsx
@@ -1,5 +1,13 @@
+import WorkerApplications from "../features/applications/components/WorkerApplications";
 function Applications() {
-  return <div>Applications</div>;
+  return (
+    <div className="p-2 lg:p-0">
+      <div className="bg-black-color flex h-50 items-center justify-center rounded-2xl">
+        <h1 className="text-white">Applications </h1>
+      </div>
+      <WorkerApplications />
+    </div>
+  );
 }
 
 export default Applications;
