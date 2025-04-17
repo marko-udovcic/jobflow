01359d68db5166d59b1a89ecb4649f4a89f9be23
marko udovcic <markoudovcic53@gmail.com>
2025-04-17 11:44:07 +0200

    refactor(repositories): add methods countBy... in JobApplicationRepository, UserRepository...

--- backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/UserRepository.java
+++ backend/jobflow-api/src/main/java/com/example/jobflow_api/repositories/UserRepository.java
@@ -1,13 +1,19 @@
 package com.example.jobflow_api.repositories;
 
 import com.example.jobflow_api.models.AppUser;
+import com.example.jobflow_api.models.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, String> {
     Boolean existsByEmail(String email);
     Optional<AppUser> findByEmail(String email);
+
+    long countByCreatedAtBetweenAndRole(LocalDateTime start, LocalDateTime end, UserRole role);
+
+    long countByCreatedAtBetweenAndEnabledTrue(LocalDateTime start, LocalDateTime end);
 }
