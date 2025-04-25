187faed673291dc99643f6b3a86c431f3922593f
marko udovcic <markoudovcic53@gmail.com>
2025-04-25 12:01:27 +0200

    refactor: add all components to AdminDashboard(Statistics, UserManagment...)

--- frontend/jobflow-ui/src/pages/AdminDashboard.jsx
+++ frontend/jobflow-ui/src/pages/AdminDashboard.jsx
@@ -1,5 +1,14 @@
+import Statistics from "../features/admin-dashboard/components/Statistics";
+import CategorySection from "../features/admin-dashboard/components/CategorySection";
+import UserManagment from "../features/admin-dashboard/components/UserManagment";
 function AdminDashboard() {
-  return <div>AdminDashboard</div>;
+  return (
+    <div className="p-2 lg:p-0">
+      <Statistics />
+      <UserManagment />
+      <CategorySection />
+    </div>
+  );
 }
 
 export default AdminDashboard;
